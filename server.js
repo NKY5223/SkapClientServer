2579a10781a325663641b299202d6d73cbc879d6
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-05-05 22:07:06 +0800

    e

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const app = require("express")();
+const server = app.listen(4000);
+const WSServer = new (require("ws").Server)({ server });
+
+const fs = require("fs");
+
+const key = (function generate(len) {
+    const chars = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    let str = "";
+    for (let i = 0; i < len; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+})(32);
+console.log("Key:", key);
+
+app.get("/", (req, res) => {
+    if (req.hostname === "localhost" || req.query.key && req.query.key === key) {
+        fs.readFile("view/index.html", "utf8", (err, data) => {
+            if (err) {
+                res.send(err.toString());
+            } else {
+                res.send(data
+                    .replace("{{style}}", `<style>\n${fs.readFileSync("view/style.css")}\n</style>`)
+                    .replace("{{script}}", `<script>\n${fs.readFileSync("view/script.js")}\n</script>`)
+                );
+            }
+        });
+    } else {
+        res.status(403).send("Error 403 Forbidden");
+    }
+});
+server.on("listening", () => {
+    console.log("Listening");
+});
+
+WSServer.on("connection", ws => {
+    console.log(ws);
+});
