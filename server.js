17b9c8986edf37687beb9349203f9e08fec5633e
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-11-13 23:08:09 +0800

    restricted inputs

--- server.js
+++ server.js
@@ -35,23 +35,37 @@ WSServer.on("connection", (ws, req) => {
         const msg = msgpack.decode(new Uint8Array(e.data));
         switch (msg.e) {
             case "login":
+                if (typeof msg.username !== "string" || msg.username.length < 3 || msg.username.length > 15) {
+                    clients.splice(clients.indexOf(client), 1);
+                    broadcastClients({
+                        e: "clientLeft",
+                        username: client.username
+                    });
+                }
                 client.username = msg.username;
                 break;
             case "username":
+                if (typeof msg.username !== "string" || msg.username.length < 3 || msg.username.length > 15) {
+                    clients.splice(clients.indexOf(client), 1);
+                    broadcastClients({
+                        e: "clientLeft",
+                        username: client.username
+                    });
+                }
                 client.username = msg.username;
                 break;
             case "msg":
+                if (typeof msg.message !== "string") {
+                    clients.splice(clients.indexOf(client), 1);
+                    broadcastClients({
+                        e: "clientLeft",
+                        username: client.username
+                    });
+                }
                 broadcastClients({
                     e: "msg",
                     author: client.username,
-                    message: msg.message
-                });
-                break;
-            case "join":
-                broadcastClients({
-                    e: "msg",
-                    author: client.username,
-                    message: msg.message
+                    message: msg.message.slice(0, 200)
                 });
                 break;
             case "fuel":
@@ -60,6 +74,9 @@ WSServer.on("connection", (ws, req) => {
                     user: client.username,
                     fuel: msg.fuel
                 });
+                break;
+            case "powers":
+                break;
         }
     });
 
